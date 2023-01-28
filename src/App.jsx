74263b65c6c39490c1e344b1f2dd763c47262aa2
Alexis Ruiz <ruiz.alexis1996@gmail.com>
2023-01-28 19:01:07 +0900

    Changing style + adding the day/night background + Icons Weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { GoSearch } from "react-icons/go";
+import {
+  TiWeatherCloudy,
+  TiWeatherSunny,
+  TiWeatherSnow,
+  TiWeatherShower,
+  TiWeatherNight,
+} from "react-icons/ti";
 
 function App() {
   //States to stock the city name and the data from the API
 
   const [cityName, setCityName] = useState("");
   const [weather, setWeather] = useState({});
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${
     import.meta.env.VITE_APP_API_KEY
   }`;
 
@@ -28,23 +36,72 @@ function App() {
     }
   };
 
+  const renderSwitch = (info) => {
+    switch (info) {
+      case "Clouds":
+        return <TiWeatherCloudy />;
+        break;
+      case "Clear":
+        return weather.dt > weather.sys.sunrise &&
+          weather.dt < weather.sys.sunset ? (
+          <TiWeatherSunny />
+        ) : (
+          <TiWeatherNight />
+        );
+        break;
+      case "Snow":
+        return <TiWeatherSnow />;
+        break;
+      case "Rain":
+        return <TiWeatherShower />;
+      default:
+        console.log("No value found");
+    }
+  };
+
   return (
-    <div>
-      <div className="navbar">
-        <h1>The Weather App</h1>
-        <input
-          type="text"
-          placeholder="City"
-          onChange={(e) => setCityName(e.target.value)}
-          onKeyDown={fetchCity}
-        />
-      </div>
+    <div
+      className="main"
+      style={
+        weather.weather
+          ? weather.dt > weather.sys.sunrise && weather.dt < weather.sys.sunset
+            ? {
+                background:
+                  "radial-gradient(ellipse at 50% 50%, #44b3f2 0%, #52bcc7 100%)",
+              }
+            : {
+                background:
+                  "radial-gradient(ellipse at 50% 50%, #22007c 0%, #140152 100%)",
+                color: "white",
+              }
+          : null
+      }>
+      <nav className="navbar">
+        <div className="nav--search">
+          <GoSearch className="input--icon" />
+          <input
+            className="nav--input"
+            type="text"
+            placeholder="City"
+            onChange={(e) => setCityName(e.target.value)}
+            onKeyDown={fetchCity}
+          />
+        </div>
+      </nav>
       <div className="main">
-        <h1>Here goes the result</h1>
-        <h2>{weather.name}</h2>
-        <div>{weather.main ? <h2>{weather.main.temp}</h2> : null}</div>
+        <h1>{weather.name}</h1>
         <div>
-          {weather.weather ? <h2>{weather.weather[0].description}</h2> : null}
+          {weather.main ? (
+            <h2 className="main--temp">{Math.round(weather.main.temp)} °C</h2>
+          ) : null}
+        </div>
+        <div className="main--icon">
+          {weather.weather ? renderSwitch(weather.weather[0].main) : null}
+        </div>
+        <div>
+          {weather.weather ? (
+            <h2>{weather.weather[0].description.toUpperCase()}</h2>
+          ) : null}
         </div>
       </div>
     </div>
